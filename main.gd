7c6171257662a84623340d861e4ba7f6f00762b9
4lugo7 <156713804+4lugo7@users.noreply.github.com>
2026-04-27 17:03:54 +0200

    aadad

--- main.gd
+++ main.gd
@@ -83,7 +83,7 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed('ui_text_backspace'):
 		delete_pressed()
 	
-
+#dsdsd
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.keycode in range(65, 91):
 		if event.pressed:
